2c04f586ad8ab90e02806a8b2d089040fd7659bd
tchatua <tchattua@gmail.com>
2022-12-16 21:01:14 -0500

    Modify a02

---
+++ Code_Scripting/a00_git_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+
+echo ""
+git add .
+# git commit -m "Modify a02"
+git commit -m "Modify a02"
+git push
--- Code_Scripting/a02_First_Script.sh
+++ Code_Scripting/a02_First_Script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+clear
 
 echo "Welcome to a quick refresh bash script son"
 echo 
 echo "The uptime of the system is: "
 uptime
+echo
 echo "Memory Utilization: "
 free -m
+echo
 echo "Disk Utilization: "
-df -h 
+df -h
+echo 
