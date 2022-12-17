548ff3d143f27039ee58c5eebb986e53dc978c8a
tchatua <tchattua@gmail.com>
2022-12-16 20:55:06 -0500

    Add a02

---
+++ Code_Scripting/a01_vagrant.sh
@@ -0,0 +1,13 @@
+vagrant up
+vagrant global-status
+vagrant ssh scriptbox
+
+#
+vi /etc/hostname
+    # Delete the file content
+hostname scriptbox
+    # logout and login back
+hostname
+#
+yum install vim -y 
+yum install git -y 
---
+++ Code_Scripting/a02_First_Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Welcome to a quick refresh bash script son"
+echo 
+echo "The uptime of the system is: "
+uptime
+echo "Memory Utilization: "
+free -m
+echo "Disk Utilization: "
+df -h 
